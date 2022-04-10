19dac950318c7154efdc1677c11b39cf743d4091
Ricardo García <ricardo.garcruz@gmail.com>
2022-04-09 23:46:54 -0500

    Sugerencias added

--- src/app/paises/pages/por-pais/por-pais.component.ts
+++ src/app/paises/pages/por-pais/por-pais.component.ts
@@ -6,16 +6,24 @@ import { Country } from '../../interfaces/paises-interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li{
+      cursor: pointer;
+    }
+    `
   ]
 })
 export class PorPaisComponent {
   termino: string = '';
   errorExists: boolean = false;
+  mostrarSugerencias: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
   //private apiUrl: string = 'https://restcountries.eu/rest/v2';
   private apiUrl: string = 'https://restcountries.com/v2';
 
-  buscar(termino: string){    
+  buscar(termino: string){
+    this.mostrarSugerencias = false;
     this.errorExists= false;
     this.termino = termino;
     this.paisService.buscarPais(this.termino)
@@ -30,8 +38,17 @@ export class PorPaisComponent {
 
   sugerencias(termino: string){
     this.errorExists = false;
-      
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    this.paisService.buscarPais(termino)
+      .subscribe(paises => this.paisesSugeridos= paises.splice(0,5),
+      (err) => this.paisesSugeridos = []);
+    }
+    
+  buscarSugerido(termino: string){
+    this.buscar(termino);
   }
 
   constructor(private paisService: PaisService){}
+
 }
